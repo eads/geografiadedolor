ecd51f00f2eb867fe6bc4821b08c73f77523e71a
mago torres <magotorres@magos-MacBook-Air.local>
2014-08-05 09:16:53 -0500

    Created geografiadedolor from https://github.com/newsapps/tarbell-template

---
+++ tarbell_config.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tarbell project configuration
+"""
+
+# Short project name
+NAME = "geografiadedolor"
+
+# Descriptive title of project
+TITLE = "Geografia de dolor"
+
+# Google spreadsheet key
+SPREADSHEET_KEY = "117-hc2v8NGBTH9G7KGOConXePCTCNXb7otVE92Z9g1U"
+
+# Exclude these files from publication
+EXCLUDES = ["*.md", "requirements.txt"]
+
+# Spreadsheet cache lifetime in seconds. (Default: 4)
+# SPREADSHEET_CACHE_TTL = 4
+
+# Create JSON data at ./data.json, disabled by default
+# CREATE_JSON = True
+
+# Get context from a local file or URL. This file can be a CSV or Excel
+# spreadsheet file. Relative, absolute, and remote (http/https) paths can be 
+# used.
+# CONTEXT_SOURCE_FILE = ""
+
+# EXPERIMENTAL: Path to a credentials file to authenticate with Google Drive.
+# This is useful for for automated deployment. This option may be replaced by
+# command line flag or environment variable. Take care not to commit or publish
+# your credentials file.
+# CREDENTIALS_PATH = ""
+
+# S3 bucket configuration
+#S3_BUCKETS = {
+    # Provide target -> s3 url pairs, such as:
+    #     "mytarget": "mys3url.bucket.url/some/path"
+    # then use tarbell publish mytarget to publish to it
+#}
+
+# Default template variables
+DEFAULT_CONTEXT = {
+    'name': 'geografiadedolor',
+    'title': 'Geografia de dolor'
+}
