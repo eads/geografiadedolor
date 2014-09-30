a61fae7849541735e41469fe0f55b22e65ca24a2
David Eads <davideads@gmail.com>
2014-09-29 23:51:36 -0400

    multilingual social media

---
+++ js/multilingual_social_media.js
@@ -0,0 +1,27 @@
+parseQueryString = function (query) {
+    var match,
+        params = {},
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); };
+
+    while (match = search.exec(query))
+       params[decode(match[1])] = decode(match[2]);
+
+   return params;
+};
+
+var shareLang = function(e) {
+  e.stopPropagation();
+  var langFull = navigator.language || navigator.userLanguage,
+      lang = langFull.slice(0, 2),
+      url_parts = $(this).attr('href').split('?'),
+      url = url_parts[0],
+      params = parseQueryString(url_parts[1]);
+
+  params.text = $(this).data('share-' + lang);
+  this.href = url + '?' + $.param(params);
+  window.open(this.href, '_blank');
+}
+
+$('.nav-share').on('click', shareLang);
